edc286097af090130b8529c458218fc7e300dd71
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2022-01-21 17:40:02 +0530

    removed not used files

--- text/cleaners.py
+++ text/cleaners.py
@@ -14,8 +14,6 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
 
 import re
 from unidecode import unidecode
-from .numbers import normalize_numbers
-
 
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r'\s+')
@@ -48,14 +46,8 @@ def expand_abbreviations(text):
     text = re.sub(regex, replacement, text)
   return text
 
-
-def expand_numbers(text):
-  return normalize_numbers(text)
-
-
 def lowercase(text):
   return text.lower()
-
 
 def collapse_whitespace(text):
   return re.sub(_whitespace_re, ' ', text)
@@ -76,15 +68,5 @@ def transliteration_cleaners(text):
   '''Pipeline for non-English text that transliterates to ASCII.'''
   text = convert_to_ascii(text)
   text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
-
-
-def english_cleaners(text):
-  '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
   text = collapse_whitespace(text)
   return text
