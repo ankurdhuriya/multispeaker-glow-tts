6f6be6c02f4817433580c685cb1a8d6b0817e564
Ankur Dhuriya <mail.ankurdhuriya@gmail.com>
2022-01-28 09:40:37 +0530

    Update train_ddi.sh

--- train_ddi.sh
+++ train_ddi.sh
@@ -1,5 +1,10 @@
 config=$1
 modeldir=$2
 
-python init.py -c $config -m $modeldir
-python train.py -c $config -m $modeldir
+init=1 # 1, start from scratch - 0, start from last checkpoint
+
+if [[ $init -eq 1 ]]
+then
+  python ./init.py -c $config -m $modeldir -l $logdir
+fi
+python ./train.py -c $config -m $modeldir -l $logdir
